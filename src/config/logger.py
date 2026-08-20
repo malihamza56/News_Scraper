df3f9d74064614abb764f4b1d0ce363907c0d006
M.Ali Hamza <mohammadalihamza97@gmail.com>
2026-08-20 12:31:14 +0500

    News Scraper: Logging Setup

---
+++ src/config/logger.py
@@ -0,0 +1,22 @@
+
+"""
+LOGGING MODULE: AN INStANCE WHICH LOG THE ERRORS AND INFOS OF ALL MODULES
+"""
+
+
+import logging
+from src.config.config import LOGGING_FILE_PATH
+
+#!BASIC CONFIG
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename=LOGGING_FILE_PATH,
+    filemode='w',
+    format="%(asctime)s | %(levelname)s | %(filename)s | %(message)s"   
+)
+
+
+#*LOGGER INSTANCE
+
+logger = logging.getLogger(__name__)
